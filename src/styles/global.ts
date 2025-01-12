6fdff4d79c651f59fcd292cd0c7bcee5a72d5002
quint <quint@neflyx.com>
2025-01-12 23:43:20 +0100

    font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,13 +3,14 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
     @font-face {
         font-family: pixelfont;
-        src: url('/fonts/technofosiano.ttf');
+        src: url('/fonts/IBM/WebPlus_IBM_VGA_8x16.woff');
     }
     
     body {
         background-color: #111;
         //color: rgba(0, 140, 255, 0.8);
-        font-family: monospace;
+        font-family: pixelfont, monospace;
+        font-size: 16px;
         margin: 0;
         padding: 0;
         font-smooth: never;
