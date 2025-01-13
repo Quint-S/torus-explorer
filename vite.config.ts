35a6a258f1ff3873fce8cf8318be4bbab7528b0f
quint-s <quint@neflyx.com>
2025-01-13 10:03:58 +0100

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-  },
   base: "",
   css: {
     postcss: './postcss.config.js', // If you're using PostCSS
