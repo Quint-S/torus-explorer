6484807c0799f8ef9e57640b2ed2061279f66569
quint <quint@neflyx.com>
2025-02-01 15:48:13 +0100

    testnet

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import App from './App'
 import { GlobalStyle } from './styles/global'
-import './index.css';  // Add this line
+import './index.css'; 
 
+const getApolloClient = () => {
+  const isTestnet = window.location.host.startsWith('test')
+  return new ApolloClient({
+    uri: isTestnet 
+      ? 'https://api.subquery.network/sq/torus-explorer/torus-test-net'
+      : 'https://api.subquery.network/sq/CommuneExplorer/torus-indexer',
+    cache: new InMemoryCache(),
+  })
+}
+console.log(window.location.host, 'LOC')
 
-const client = new ApolloClient({
-  uri: 'https://api.subquery.network/sq/CommuneExplorer/torus-indexer',
-  cache: new InMemoryCache(),
-})
+const client = getApolloClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
